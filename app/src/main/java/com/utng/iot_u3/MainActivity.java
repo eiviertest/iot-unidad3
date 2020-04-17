6cc46e2191c3bc41b59869d005d43333d9999163
AlejandroVargas <32745553+eiviertest@users.noreply.github.com>
2020-04-17 16:08:26 -0500

    Documentación de código

--- app/src/main/java/com/utng/iot_u3/MainActivity.java
+++ app/src/main/java/com/utng/iot_u3/MainActivity.java
@@ -16,6 +16,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Instancia de objecto; uso de plantilla, de manera global a la clase
     FirebaseDatabase database = FirebaseDatabase.getInstance();
 
     @Override
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Conseguir los item del XML relacionado con esta clase por medio de su propiedad ID
         TextView led_on_sala = findViewById(R.id.led_on_sala);
         TextView led_off_sala = findViewById(R.id.led_off_sala);
         TextView led_on_cocina = findViewById(R.id.led_on_cocina);
@@ -31,13 +33,16 @@ public class MainActivity extends AppCompatActivity {
         TextView led_off_mirador = findViewById(R.id.led_off_mirador);
         TextView led_on_pasillo = findViewById(R.id.led_on_pasillo);
         TextView led_off_pasillo = findViewById(R.id.led_off_pasillo);
+        //Caracteristica final agregada para que sean unico e irrepetibles en la clase
         final TextView txt_sensorUltrasonico = findViewById(R.id.txt_sensorUltrasonico);
         final TextView txt_sensorMovimiento = findViewById(R.id.txt_sensorMovimiento);
         final TextView txt_sensorSonido = findViewById(R.id.txt_sensorSonido);
 
+        //Método para cuando hay click en ese item, se cree una referencia y colocarle un valor
         led_on_cocina.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Instacia de un campo desde la base de datos
                 DatabaseReference myRef = database.getReference("led_cocina_status");
                 myRef.setValue(1);
             }
@@ -62,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
         led_off_sala.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
                 DatabaseReference myRef = database.getReference("led_sala_status");
                 myRef.setValue("Apagado");
             }
@@ -100,14 +104,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Detectar si hay algun cambio en la base de datos sobre el campo especificado
         DatabaseReference myRefUltrasonico = database.getReference("distanciaUltrasonico");
         myRefUltrasonico.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //Casteo de un objeto a tipo de datos cadena
                 String change = dataSnapshot.getValue().toString();
                 txt_sensorUltrasonico.setText("Hay un objecto a " + change + " cm");
-                if (Integer.parseInt(change) < 30) {
+                if (Integer.parseInt(change) < 30) { //Conversion de cadena a entero
+                    //Instancia de campo proveniente de la BD
                     DatabaseReference myRefAlarma = database.getReference("alarma_status");
+                    //Colocacion del valor a la referencia (campo)
                     myRefAlarma.setValue(1);
                 } else {
                     DatabaseReference myRefAlarma = database.getReference("alarma_status");
